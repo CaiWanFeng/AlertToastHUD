575f3baf22f9655bab8a08a4b737831b6eb1a163
caiqiang <caiqiang@kuaijiankang.com>
2017-06-07 17:24:50 +0800

    添加toast

---
+++ 弹窗哈哈哈/CQPointHUD.h
@@ -0,0 +1,19 @@
+//
+//  CQPointHUD.h
+//  弹窗哈哈哈
+//
+//  Created by 蔡强 on 2017/6/7.
+//  Copyright © 2017年 kuaijiankang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CQPointHUD : UIView
+
+/** 纯文本toast提示 */
++ (void)showToastWithMessage:(NSString *)message;
+
+/** 图文toast提示 */
++ (void)showToastWithMessage:(NSString *)message image:(NSString *)imageName;
+
+@end
--- 弹窗哈哈哈/ViewController.m
+++ 弹窗哈哈哈/ViewController.m
@@ -8,6 +8,7 @@
 
 #import "ViewController.h"
 #import "DeclareAbnormalAlertView.h"
+#import "CQPointHUD.h"
 
 @interface ViewController ()<DeclareAbnormalAlertViewDelegate>
 
@@ -19,18 +20,42 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    // 创建一个按钮
+    // 自定义弹窗按钮
     UIButton *button = [[UIButton alloc]initWithFrame:CGRectMake(90, 90, 190, 30)];
     [self.view addSubview:button];
     button.backgroundColor = [UIColor redColor];
     [button setTitle:@"点击弹出弹窗" forState:UIControlStateNormal];
     [button addTarget:self action:@selector(showAlert) forControlEvents:UIControlEventTouchDown];
+    
+    // 纯文本toast按钮
+    UIButton *button2 = [[UIButton alloc]initWithFrame:CGRectMake(90, 150, 190, 30)];
+    [self.view addSubview:button2];
+    button2.backgroundColor = [UIColor blueColor];
+    [button2 setTitle:@"纯文本toast" forState:UIControlStateNormal];
+    [button2 addTarget:self action:@selector(showToast) forControlEvents:UIControlEventTouchUpInside];
+    
+    // 图文toast按钮
+    UIButton *button3 = [[UIButton alloc]initWithFrame:CGRectMake(90, 210, 190, 30)];
+    [self.view addSubview:button3];
+    button3.backgroundColor = [UIColor orangeColor];
+    [button3 setTitle:@"图文toast" forState:UIControlStateNormal];
+    [button3 addTarget:self action:@selector(showImageToast) forControlEvents:UIControlEventTouchUpInside];
 }
 
 // 弹出弹窗
 - (void)showAlert{
     DeclareAbnormalAlertView *alertView = [[DeclareAbnormalAlertView alloc]initWithTitle:@"这是一个标题" message:@"长亭外，古道边，一行白鹭上青天" delegate:self leftButtonTitle:@"确定" rightButtonTitle:@"取消"];
     [alertView show];
+}
+
+// 纯文本toast
+- (void)showToast{
+    [CQPointHUD showToastWithMessage:@"您还未达到相应积分\n无法兑换商品"];
+}
+
+// 图文toast
+- (void)showImageToast{
+    [CQPointHUD showToastWithMessage:@"兑换成功" image:@"sign"];
 }
 
 // 弹窗的代理方法
