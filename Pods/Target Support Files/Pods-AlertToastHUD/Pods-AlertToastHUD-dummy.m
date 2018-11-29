181c45c2e71a7eb116d71ddf795329e32a4d49aa
caiqiang <caiqiang@kuaijiankang.com>
2018-11-29 16:23:50 +0800

    修改项目名

---
+++ Pods/Target Support Files/Pods-AlertToastHUD/Pods-AlertToastHUD-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AlertToastHUD : NSObject
+@end
+@implementation PodsDummy_Pods_AlertToastHUD
+@end
--- Pods/Target Support Files/Pods-弹窗哈哈哈/Pods-弹窗哈哈哈-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods______ : NSObject
-@end
-@implementation PodsDummy_Pods______
-@end
