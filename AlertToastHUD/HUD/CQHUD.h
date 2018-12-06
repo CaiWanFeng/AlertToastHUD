c3d935caf673c322c996096cf95458ea13fac286
caiqiang <caiqiang@kuaijiankang.com>
2018-12-06 16:17:51 +0800

    移除对RAC的依赖

--- AlertToastHUD/HUD/CQHUD.h
+++ AlertToastHUD/HUD/CQHUD.h
@@ -10,32 +10,9 @@
 
 @interface CQHUD : UIView
 
-
-#pragma mark - 带block回调的弹窗
-/** 带block回调的弹窗 */
-+ (void)showAlertWithButtonClickedBlock:(void(^)())buttonClickedBlock;
-
-
-#pragma mark - 带网络图片与block回调的弹窗
-/** 带网络图片与block回调的弹窗 */
-+ (void)showAlertWithImageURL:(NSString *)imageURL ButtonClickedBlock:(void(^)())buttonClickedBlock;
-
-
-#pragma mark - 炫彩弹窗
-/**
- 兑换成功后展示的弹窗
- 
- @param couponName 优惠券名称
- @param validityTime 有效期
- @param checkButtonClickedBlock “查看优惠券”按钮点击后的回调
- */
-+ (void)showConversionSucceedAlertWithCouponName:(NSString *)couponName validityTime:(NSString *)validityTime checkCouponButtonClickedBlock:(void(^)())checkButtonClickedBlock;
-
-
 #pragma mark - 展示loading图
 /** 展示loading图 */
 + (void)showLoading;
-
 
 #pragma mark - 展示带说明信息的loading图
 /**
@@ -45,11 +22,9 @@
  */
 + (void)showLoadingWithMessage:(NSString *)message;
 
-
 #pragma mark - 移除loading图
 /** 移除loading图 */
 + (void)dismiss;
-
 
 #pragma mark - loading期间，允许或禁止用户交互
 /**
@@ -59,7 +34,6 @@
  */
 + (void)enableUserInteraction:(BOOL)isEnable;
 
-
 #pragma mark - 展示可控制用户交互的loading图
 /**
  展示可控制用户交互的loading图
@@ -67,7 +41,6 @@
  @param isEnable 是否允许用户交互
  */
 + (void)showLoadingWithEnableUserInteraction:(BOOL)isEnable;
-
 
 #pragma mark - 展示可控制用户交互并且带说明信息的loading图
 /**
@@ -77,6 +50,5 @@
  @param isEnable 是否允许用户交互
  */
 + (void)showLoadingWithMessage:(NSString *)message enableUserInteraction:(BOOL)isEnable;
-
 
 @end
