3bf75284a790a86347945c4048be6365ba9ed570
caiqiang <caiqiang@kuaijiankang.com>
2018-12-07 23:49:00 +0800

    添加微信菜单弹窗

---
+++ AlertToastHUD/Alert/微信菜单弹窗/CQWeChatMenuView.h
@@ -0,0 +1,13 @@
+//
+//  CQWeChatMenuView.h
+//  AlertToastHUD
+//
+//  Created by caiqiang on 2018/12/7.
+//  Copyright © 2018年 kuaijiankang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CQWeChatMenuView : UIView
+
+@end
---
+++ AlertToastHUD/Alert/微信菜单弹窗/CQWeChatMenuView.m
@@ -0,0 +1,15 @@
+//
+//  CQWeChatMenuView.m
+//  AlertToastHUD
+//
+//  Created by caiqiang on 2018/12/7.
+//  Copyright © 2018年 kuaijiankang. All rights reserved.
+//
+
+#import "CQWeChatMenuView.h"
+
+@implementation CQWeChatMenuView
+
+
+
+@end
